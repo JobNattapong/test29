9ca7aeab9e0daf30d640971628b69a1b18734c59
jobnattapong <s5701012620187@email.kmutnb.ac.th>
2018-06-18 00:12:25 +0700

    Unittest test root and url index

--- quiz/tests.py
+++ quiz/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from django.urls import resolve
+from quiz.views import index
+import datetime
+from django.utils import timezone
+from quiz.models import Question, Choice
 
-# Create your tests here.
+class IndexTest(TestCase):
+    def test_root_index(self):
+        fonnd = resolve('/quiz/')
+        self.assertEqual(fonnd.func, index)
+
+    def test_user_index_template(self):
+        response = self.client.get('/quiz/')
+        self.assertTemplateUsed(response, 'quiz/index.html')
